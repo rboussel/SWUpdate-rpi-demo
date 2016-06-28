6cc7376e9b8a932f1a4a4950c880df4bdedaa252
Roumaissa Boussellaoui <roumaissa.boussellaoui@smile.fr>
2016-06-28 08:22:31 +0000

    Add rpi and u-boot scripts

---
+++ rpi-scripts/swu_state_machine.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+UPDATE_DIR="/DATA/update"
+EXTENTION="swu"
+PUBLIC_KEY_PATH="${UPDATE_DIR}/public.pem"
+UPDATE_NAME="swupdate_1.0.swu"
+R="2"
+
+retry_next () {
+  fw_setenv test_validity "true"
+  fw_setenv retry_count "$R"
+}
+
+decrement_variable () {
+  new_value=$(fw_printenv $1 | cut -d= -f2)
+  let new_value--
+  fw_setenv $1 $new_value 
+  echo $new_value
+}
+
+find_fs () {
+  current_part=$(fw_printenv "part" | cut -d= -f2)
+  if [ "$current_part" -eq 0 ]
+  then
+    UPDATED_PARTITION="rootfs,alt"
+  else
+    UPDATED_PARTITION="rootfs,main"
+  fi
+  echo "Update root file system on ${UPDATED_PARTITION}"
+}
+
+find_app () {
+  current_part=$(fw_printenv "appli" | cut -d= -f2)
+  if [ "$current_part" -eq 0 ]
+  then
+    UPDATED_PARTITION="application,alt"
+  else
+    UPDATED_PARTITION="application,main"
+  fi
+  echo "Update application on ${UPDATED_PARTITION}"
+}
+
+lauch_update () {
+  UPDATED_PARTITION=$(find_fs)
+  swupdate -k ${PUBLIC_KEY_PATH} -H raspb:revA -e ${UPDATED_PARTITION} -vi "${UPDATE_DIR}/${UPDATE_NAME}"
+  #verif if ok 
+  UPDATED_PARTITION=$(find_app)
+  swupdate -k ${PUBLIC_KEY_PATH} -H raspb:revA -e ${UPDATED_PARTITION} -vi "${UPDATE_DIR}/${UPDATE_NAME}"
+}
+
+retry_update () {
+  retry_count_val=$(decrement_variable "retry_count")
+  if [ "$retry_count_val" -gt 0 ]
+  then
+    echo "UPDATE"
+    lauch_update
+  else 
+    #TODO: marquer la version comme invalide 
+    retry_next
+  fi
+}
+
+count_test () {
+  val_count=$(fw_printenv $1 | cut -d= -f2)
+  if [ "$val_count" -gt "0" ]; then retry_next ; else retry_update ; fi
+}
+
+# Specific commands to Raspberry Pi for u-boot environment
+mount /dev/mmcblk0p1 /mnt
+echo "/mnt/uboot.env 0x0000 0x4000 0x4000" > /etc/fw_env.config
+
+fw_printenv test_validity | grep "true" && retry_next || count_test "test_count"
+
