44cb543aa6d5434b9c896a9071d960c70964a182
Roumaissa Boussellaoui <roumaissa.boussellaoui@smile.fr>
2016-07-06 14:58:25 +0000

    Add current app partition and adapt function

--- overlay/opt/scripts/lauch_update.sh
+++ overlay/opt/scripts/lauch_update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+CURRENT_APPLICATION_PART_PATH="/DATA/current_application_part"
 
 find_fs () {
   current_part=$(fw_printenv "part" | cut -d= -f2)
@@ -12,25 +13,26 @@ find_fs () {
 }
 
 find_app () {
-  current_part=$(fw_printenv "appli" | cut -d= -f2)
-  if [ "$current_part" -eq 0 ]
-  then
+  current_partition_part=$(cat $CURRENT_APPLICATION_PART_PATH)
+  if [ $current_partition_part == "/dev/mmcblk0p5" ]
+  then 
     UPDATED_PARTITION="application,alt"
   else
     UPDATED_PARTITION="application,main"
-  fi
   echo "${UPDATED_PARTITION}"
 }
 
 lauch_update () {
   mount /dev/mmcblk0p1 /mnt
+  mount /dev/mmcblk0p7 /DATA
   UPDATED_PARTITION=$(find_fs)
-  swupdate -k ${PUBLIC_KEY_PATH} -e ${UPDATED_PARTITION} -vi "${UPDATE_DIR}/$1"
+  swupdate -k ${PUBLIC_KEY_PATH} -e ${UPDATED_PARTITION} -vi "${UPDATE_DIR}/$ROOTFS_UPDATE_NAME"
   #verif if ok 
   UPDATED_PARTITION=$(find_app)
-  swupdate -k ${PUBLIC_KEY_PATH} -e ${UPDATED_PARTITION} -vi "${UPDATE_DIR}/$1"
+  swupdate -k ${PUBLIC_KEY_PATH} -e ${UPDATED_PARTITION} -vi "${UPDATE_DIR}/$APPLICATION_UPDATE_NAME"
   umount /dev/mmcblk0p1
+  umount /dev/mmcblk0p7
 }
 
-lauch_update $1
+lauch_update 
 
