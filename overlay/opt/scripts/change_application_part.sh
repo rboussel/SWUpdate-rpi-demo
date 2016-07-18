1825745e43156583063e71ed77e2b47d1b276197
Roumaissa Boussellaoui <roumaissa.boussellaoui@smile.fr>
2016-07-18 15:22:02 +0000

    Change file

--- overlay/opt/scripts/change_application_part.sh
+++ overlay/opt/scripts/change_application_part.sh
@@ -8,7 +8,7 @@ else
   CURRENT_APP_PART=$(cat $CONFIG_DATA | sed -n '/main_partition=/p' | cut -d= -f2)   
 fi
 
-echo $CURRENT_APP_PART > $CONFIG_DATA
+echo $CURRENT_APP_PART > "/DATA/current_application_part"
 }
 #modifier la valeur dans env_var
 change_partition
