b6a4b45061107553528ede77b2cec14136c9c1a8
Roumaissa Boussellaoui <roumaissa.boussellaoui@smile.fr>
2016-08-02 12:12:23 +0000

    Add function

--- overlay/opt/scripts/change_application_part.sh
+++ overlay/opt/scripts/change_application_part.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # change_application_part.sh - Change application part in current_partition_part and env_var files
+
 change_partition () {
 if [ $CURRENT_APP_PART == $(cat $CONFIG_DATA | sed -n '/main_partition=/p' | cut -d= -f2) ]
 then 
@@ -10,4 +11,18 @@ fi
 
 echo $CURRENT_APP_PART > "/DATA/current_application_part"
 }
-change_partition
+
+
+get_temp_part () {
+if [ $CURRENT_APP_PART == $(cat $CONFIG_DATA | sed -n '/main_partition=/p' | cut -d= -f2) ]
+then 
+  echo $(cat $CONFIG_DATA | sed -n '/alt_partition=/p' | cut -d= -f2) 
+else
+  echo $(cat $CONFIG_DATA | sed -n '/main_partition=/p' | cut -d= -f2)   
+fi
+}
+
+case $1 in 
+  "change") change_partition ;;
+  "temp") echo $(get_temp_part);;
+  esac
