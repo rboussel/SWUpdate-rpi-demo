0f6a1416b598d04eb43605804f661195edef5738
Roumaissa Boussellaoui <roumaissa.boussellaoui@smile.fr>
2016-08-11 13:37:23 +0000

    Add comments

--- swupdate/create-archive.sh
+++ swupdate/create-archive.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-#Script to create a signed .swu archive due to the sw-description file.
+# Script to create a signed .swu archive due to the sw-description file.
+# All files specified in sw-description need to be in SWUPDATE_FILES_PATH
+# except images which are in BINARIES_DIR
+# This script does not create sw-description
 
-CONTAINER_VER="1.0"
+# Variables
+VERSION="1.0"
 EMERGENCY_WORD="CRITICAL"
 PRODUCT_NAME="rpi_swupdate"
 SWUPDATE_FILES_PATH="${BR2_EXTERNAL}/swupdate"
 FILES="sw-description sw-description.sig"
 
+# Add hash in sw-description. 
 compute_hash(){
 	names=$(cat "${SWUPDATE_FILES_PATH}/sw-description" | sed -n '/@/p' | cut -d@ -f2 | uniq )
 	cp ${SWUPDATE_FILES_PATH}/sw-description ${BINARIES_DIR}/sw-description ;
@@ -17,6 +22,7 @@ compute_hash(){
 	sed -i "s/@$name/\"$sha256\";/"  "${BINARIES_DIR}/sw-description" ; done ;
 }
 
+# Create sw-description.sig. A private key is needed
 create_swdescription_sig(){
 	if test -f "${SWUPDATE_FILES_PATH}/priv.pem"
 	then
@@ -27,13 +33,14 @@ create_swdescription_sig(){
 	fi
 }
 
+# Create .swu archive 
 create_swu(){
 	cd ${BINARIES_DIR}
 	for file in $FILES; do
-		echo $file;done | cpio -ov -H crc >  ${PRODUCT_NAME}-${CONTAINER_VER}-${EMERGENCY_WORD}.swu
+		echo $file;done | cpio -ov -H crc >  ${PRODUCT_NAME}-${VERSION}-${EMERGENCY_WORD}.swu
   if [ -f "${PRODUCT_NAME}-${CONTAINER_VER}-${EMERGENCY_WORD}.swu" ]
   then 
-    echo "L'archive ${PRODUCT_NAME}-${CONTAINER_VER}-${EMERGENCY_WORD}.swu a bien été créée dans ${BINARIES_DIR}"
+    echo "L'archive ${PRODUCT_NAME}-${VERSION}-${EMERGENCY_WORD}.swu a bien été créée dans ${BINARIES_DIR}"
   fi
 
 	rm sw-description sw-description.sig
